572faf9e3cb2b46a4932e401ef347551a8e7a1b8
vichyssoise <v@vichyssoise.io>
2018-03-17 22:38:11 +0900

    Add page code template

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,35 @@
+import * as React from 'react'
 import Link from 'next/link'
 
-export default () =>
-  <div>
-    Hello World.{' '}
-    <Link href="/about">
-      <a>About</a>
-    </Link>
-  </div>
+interface props {
+  name: string
+}
+interface state {
+  toggle: boolean
+}
+export default class Index extends React.Component<props, state> {
+  static getInitialProps({ req }) {
+    return { name: 'me!' }
+  }
+
+  state = {
+    toggle: true
+  }
+
+  handleClick = () => {
+    this.setState({ toggle: !this.state.toggle })
+  }
+
+  getBackgroundColor = () => (this.state.toggle ? '#ff0000' : '#00ff00')
+
+  render() {
+    return (
+      <div style={{ backgroundColor: this.getBackgroundColor() }} onClick={this.handleClick}>
+        Click {this.props.name}.{' '}
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+      </div>
+    )
+  }
+}
